95643531bd1d9a914d7fd18bd462857aefdfa5c0
meg-twilio <mcummings@twilio.com>
2020-02-25 16:54:28 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser') //Used to parse requests so you can use its info
+const app = express()
+const routes = require("./routes")
+const PORT = process.env.PORT || 8080;
+
+// --------------------- MIDDLEWARE --------------------- //
+
+app.use(bodyParser.urlencoded({ extended: false }));// Thus lets express know to use static files
+
+// --------------------- ROUTES --------------------- //
+app.get('/', (request, response) => {
+	response.send('<h1>Welcome to Twilio</h1>');
+});
+
+
+
+
+
+// --------------------- START SERVER --------------------- //
+app.listen(PORT, function() {
+	console.log(`MG App Started on Port ${PORT}`)
+});
