61d0265a1734ada35fc2b14f87f021f2863004ad
meg-twilio <mcummings@twilio.com>
2020-02-27 15:22:08 -0800

    added doc code

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
 require('dotenv').config()
 const express = require('express')
-const bodyParser = require('body-parser') //Used to parse requests so you can use its info
-const app = express()
-const routes = require("./routes")
 const PORT = process.env.PORT || 8080;
+const twilio = require('twilio')
+const client = new twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN) 
 
 // --------------------- MIDDLEWARE --------------------- //
 
-app.use(bodyParser.urlencoded({ extended: false }));// Thus lets express know to use static files
+const MODERATOR = '+19045371699'
+
+const app = express();
+
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const urlencoded = require('body-parser').urlencoded; //Used to parse requests so you can use its info
+
+app.use(urlencoded({ extended: false }));// Thus lets express know to use static files
 
 // --------------------- ROUTES --------------------- //
 app.get('/', (request, response) => {
 	response.send('<h1>Welcome to Twilio</h1>');
 });
 
+app.post('/voice', (request, response) => {
+	const twiml = new VoiceResponse();
 
+	const dial = twiml.dial();
 
+	if (request.body.From == MODERATOR) {
+		dial.conference('My conference', {
+			startConferenceOnEnter: true,
+			endConferenceOnExit: true,
+		});
+	} else {
+		dial.conference('My conference', {
+			startConferenceOnEnter: false,
+		});
+	};
 
+	response.type('text/xml');
+	response.send(twiml.toString());
+})
 
 // --------------------- START SERVER --------------------- //
 app.listen(PORT, function() {
-	console.log(`MG App Started on Port ${PORT}`)
+	console.log(`Twilio Client app HTTP server running on Port ${PORT}`)
 });
